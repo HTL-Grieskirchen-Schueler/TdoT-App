a8211572b6f98ccdb09750c37bce5916957bc77b
Jakob WINTERSTEIGER <WintersteigerJ190225@sus.htl-grieskirchen.at>
2024-10-08 08:30:51 +0200

    add navigation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tdot_gkr/screens/navigation.dart';
 import '../widgets/activity.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
@@ -15,6 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Center(
         child: Column(
           children: <Widget>[
+            // Logo Section
             Expanded(
               flex: 1,
               child: Container(
@@ -25,6 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
+            // Main Content Section
             Expanded(
               flex: 4,
               child: Container(
@@ -40,34 +44,41 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 child: Column(
                   children: <Widget>[
+                    // SVG Wave Image
                     SvgPicture.asset(
                       "assets/wave.svg",
                       fit: BoxFit.fitWidth,
                       alignment: Alignment.topCenter,
                     ),
+                    // Activity Section
                     Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <ActivityWidget>[
-                          ActivityWidget(
-                            title: "Wegweiser",
-                            description: "lorem lorem lorem lorem lorem lorem",
-                            iconData: Icons.map,
-                            onTap: () => print("Option 1 tapped"),
-                          ),
-                          ActivityWidget(
-                            title: "Anmelden",
-                            description: "Option 2",
-                            iconData: Icons.school,
-                            onTap: () => print("Option 2 tapped"),
-                          ),
-                          ActivityWidget(
-                            title: "Infos",
-                            description: "Option 3",
-                            iconData: Icons.info,
-                            onTap: () => print("Option 3 tapped"),
-                          ),
-                        ],
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 180.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            ActivityWidget(
+                              title: "Wegweiser",
+                              description:
+                                  "lorem lorem lorem lorem lorem lorem",
+                              iconData: Icons.map,
+                              onTap: () => _navigateTo(
+                                  context, const NavigationScreen()),
+                            ),
+                            ActivityWidget(
+                              title: "Anmelden",
+                              description: "Option 2",
+                              iconData: Icons.school,
+                              onTap: () => print("Option 2 tapped"),
+                            ),
+                            ActivityWidget(
+                              title: "Infos",
+                              description: "Option 3",
+                              iconData: Icons.info,
+                              onTap: () => print("Option 3 tapped"),
+                            ),
+                          ],
+                        ),
                       ),
                     )
                   ],
@@ -76,6 +87,14 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  void _navigateTo(BuildContext context, Widget screen) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => screen,
       ),
     );
   }
