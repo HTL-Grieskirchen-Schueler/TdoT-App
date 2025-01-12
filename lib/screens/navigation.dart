cdcc5cb5db4437b061251bfdb26002aed36403fc
Jasmin Mayer <88530691+JasminMayer@users.noreply.github.com>
2025-01-12 14:49:05 +0100

    deleted:    lib/blocs/pathfinding/pathfinding.bloc.dart (#52)
    deleted:    lib/blocs/pathfinding/pathfinding.event.dart
    	deleted:    lib/blocs/pathfinding/pathfinding.state.dart
    	deleted:    lib/models/node.model.dart
    	deleted:    lib/models/pathpainter.model.dart
    	modified:   lib/screens/navigation.dart
    	modified:   pubspec.lock

--- lib/screens/navigation.dart
+++ lib/screens/navigation.dart
@@ -1,99 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-import '../blocs/pathfinding/pathfinding.bloc.dart';
-import '../blocs/pathfinding/pathfinding.event.dart';
-import '../blocs/pathfinding/pathfinding.state.dart';
-import '../models/node.model.dart';
-import '../models/pathpainter.model.dart';
 
 class NavigationScreen extends StatelessWidget {
   const NavigationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => PathfindingBloc()..add(InitializeNodesEvent()),
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Floor Plan')),
-        body: BlocBuilder<PathfindingBloc, PathfindingState>(
-          builder: (context, state) {
-            if (state is NodesInitialized) {
-              return Stack(
-                children: [
-                  SvgPicture.asset(
-                    'assets/floor_plan.svg',
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    fit: BoxFit.contain,
-                  ),
-                  CustomPaint(
-                    size: Size(
-                      MediaQuery.of(context).size.width,
-                      MediaQuery.of(context).size.height,
-                    ),
-                    painter: PathPainter([], state.nodes),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Node startNode =
-                          state.nodes.firstWhere((node) => node.label == 'top');
-                      Node endNode = state.nodes
-                          .firstWhere((node) => node.label == 'left');
-                      context
-                          .read<PathfindingBloc>()
-                          .add(FindPathEvent(startNode, endNode));
-                    },
-                    child: const Text('Find Path'),
-                  ),
-                ],
-              );
-            } else if (state is NodesInitialized || state is PathFound) {
-              List<Node> path = state is PathFound ? state.path : [];
-              List<Node> nodes = state is NodesInitialized
-                  ? state.nodes
-                  : (state as PathFound).path;
-
-              return Stack(
-                children: [
-                  SvgPicture.asset(
-                    'assets/floor_plan.svg',
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    fit: BoxFit.contain,
-                  ),
-                  CustomPaint(
-                    size: Size(
-                      MediaQuery.of(context).size.width,
-                      MediaQuery.of(context).size.height,
-                    ),
-                    painter: PathPainter(path, nodes),
-                  ),
-                  if (state is NodesInitialized)
-                    Positioned(
-                      bottom: 20,
-                      right: 20,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Node startNode = state.nodes
-                              .firstWhere((node) => node.label == 'top');
-                          Node endNode = state.nodes
-                              .firstWhere((node) => node.label == 'left');
-                          context
-                              .read<PathfindingBloc>()
-                              .add(FindPathEvent(startNode, endNode));
-                        },
-                        child: const Text('Find Path'),
-                      ),
-                    ),
-                ],
-              );
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wegweiser'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.6,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2),
+              ),
+              child: InteractiveViewer(
+                boundaryMargin: const EdgeInsets.all(100),
+                minScale: 0.5,
+                maxScale: 4.0,
+                child: SvgPicture.asset(
+                  'assets/floor_plan.svg',
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
